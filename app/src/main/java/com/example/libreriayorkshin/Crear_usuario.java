b69df4b4ac89dd422b37521058132d554c7f3b11
CamiloHenriquez <camilo.parkour@gmail.com>
2023-10-11 20:51:51 -0300

    nueva activity

---
+++ app/src/main/java/com/example/libreriayorkshin/Crear_usuario.java
@@ -0,0 +1,14 @@
+package com.example.libreriayorkshin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Crear_usuario extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_crear_usuario);
+    }
+}
