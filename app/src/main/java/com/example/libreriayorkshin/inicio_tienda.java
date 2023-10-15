f520104626563ddd42100e233d2b3ff8254dde4a
CamiloHenriquez <camilo.parkour@gmail.com>
2023-10-15 02:54:17 -0300

    inicio tienda

---
+++ app/src/main/java/com/example/libreriayorkshin/inicio_tienda.java
@@ -0,0 +1,14 @@
+package com.example.libreriayorkshin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class inicio_tienda extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_inicio_tienda);
+    }
+}
