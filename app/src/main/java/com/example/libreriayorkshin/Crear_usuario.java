d0bce41ddfa7bb7c21bf85e35f3bcdf99ac18248
CamiloHenriquez <camilo.parkour@gmail.com>
2023-10-15 06:24:49 -0300

    pruebas de fragmentos

--- app/src/main/java/com/example/libreriayorkshin/Crear_usuario.java
+++ app/src/main/java/com/example/libreriayorkshin/Crear_usuario.java
@@ -1,10 +1,14 @@
 package com.example.libreriayorkshin;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class Crear_usuario extends AppCompatActivity {
 
@@ -22,5 +26,21 @@ public class Crear_usuario extends AppCompatActivity {
         //Incorporacion del menu en la activity
         getMenuInflater().inflate(R.menu.menu,menu);
         return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+
+        int id = item.getItemId(); //Recuperar el id del item
+        if (id == R.id.op1){
+            Intent i =  new Intent(this, MainActivity.class);
+            startActivity(i);
+            Toast.makeText(this,"Seccion cerrada",Toast.LENGTH_SHORT).show();
+        }
+
+
+
+
+        return super.onOptionsItemSelected(item);
     }
 }
--- app/src/main/java/com/example/libreriayorkshin/inicio_tienda.java
+++ app/src/main/java/com/example/libreriayorkshin/inicio_tienda.java
@@ -1,9 +1,12 @@
 package com.example.libreriayorkshin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
 
 import android.os.Bundle;
 import android.widget.Toolbar;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class inicio_tienda extends AppCompatActivity {
 
@@ -11,5 +14,40 @@ public class inicio_tienda extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_inicio_tienda);
+
+
+        TabLayout tl = (TabLayout) findViewById(R.id.tablayout2);
+        tl.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
+            @Override
+            public void onTabSelected(TabLayout.Tab tab) {
+
+                int position = tab.getPosition();
+
+                switch (position) {
+                    case 0:
+                        //llamar al fragmento carga
+                        Carga p = new Carga();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.contenedor, p).commit();
+                        break;
+                    case 1:
+                        //llamar al fragmento historial
+                        historial h = new historial();
+                        getSupportFragmentManager().beginTransaction().replace(R.id.contenedor, h).commit();
+                        break;
+                }
+
+
+            }
+
+            @Override
+            public void onTabUnselected(TabLayout.Tab tab) {
+
+            }
+
+            @Override
+            public void onTabReselected(TabLayout.Tab tab) {
+
+            }
+        });
     }
 }
