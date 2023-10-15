126b72d353e35fd06da903177119a6e6a3699d18
CamiloHenriquez <camilo.parkour@gmail.com>
2023-10-15 05:18:07 -0300

    toolbar

--- app/src/main/java/com/example/libreriayorkshin/Crear_usuario.java
+++ app/src/main/java/com/example/libreriayorkshin/Crear_usuario.java
@@ -1,8 +1,10 @@
 package com.example.libreriayorkshin;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
+import android.view.Menu;
 
 public class Crear_usuario extends AppCompatActivity {
 
@@ -10,5 +12,15 @@ public class Crear_usuario extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_crear_usuario);
+
+        Toolbar tb = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(tb);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        //Incorporacion del menu en la activity
+        getMenuInflater().inflate(R.menu.menu,menu);
+        return true;
     }
 }
--- app/src/main/java/com/example/libreriayorkshin/inicio_tienda.java
+++ app/src/main/java/com/example/libreriayorkshin/inicio_tienda.java
@@ -3,6 +3,7 @@ package com.example.libreriayorkshin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Toolbar;
 
 public class inicio_tienda extends AppCompatActivity {
 
